40c2399f84f3360fb3ecb92d6b47b871a12b92ac
Petr <petrnovak@gmail.com>
2025-06-04 21:11:52 +0200

    dokončení funkčnosti generovaní textu a přidaní css

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const joksContainer = document.querySelector(".container-jokes");
+const button = document.querySelector("button");
 
 import {jokesData} from "./data.js";
 
+
+const randomJoke = () =>{
+    let randomID = Math.ceil(Math.random() * 25);
+    return jokesData.find(item =>item.id  === randomID);
+}
+
+console.log(randomJoke());
+
+button.addEventListener("click",() =>{
+    let joke = randomJoke();
+    if (joke) {
+        joksContainer.innerHTML = `<p class="joke">${joke.joke}</p>`; 
+    } else {
+        joksContainer.textContent = "Vtip nebyl nalezen!";
+    }
+})
